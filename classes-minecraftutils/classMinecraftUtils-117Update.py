99fd69516c120b9d87520022b4211572ed96f775
Ryan <43323661+RCCRAFT1@users.noreply.github.com>
2022-06-24 22:45:01 -0700

    files from 1.17

---
+++ classes-minecraftutils/classMinecraftUtils-117Update.py
@@ -0,0 +1,18 @@
+from mc import versionsVariableChecker
+from mc import versionsBoolean
+
+import os
+
+class updateBoolean(booleanObject):
+
+    async def trackUpdateTransaction(os, mc, updater):
+        
+        for key.TransmissionUpdater:
+            user.BooleanField(False)
+            versionsBoolean(checker == True)
+
+            await selfUpdater(trackUpdateTransaction, device)
+            await selfUpdater(getTransactionDate(*), wildcard == True)
+            await user.keyBooleanFailure()
+        for mc.keyTransaction()
+        
